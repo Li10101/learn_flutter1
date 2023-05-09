bbf773a617b75d0bb46aeecd1a7b3a3fdf4250ec
LiZhen <475002128@qq.com>
2023-05-09 10:59:47 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("第一个Flutter程序"),
+          centerTitle: true,
+        ),
+        body: Center(
+            child:Text(
+              "Hello World",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.orange),
+            )
+        ),
+      ),
+    )
+  );
+}
